5fc39db966112bdb9394b394f735500265a43115
Livia <livia@pop-os.localdomain>
2022-05-13 16:30:56 -0300

    Atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ambiente-angular';
+export class AppComponent implements OnInit{
+
+    count = 0;
+
+    nome = 'Livia'
+
+    text = '';
+
+    pessoas = [
+      {
+        nome: "Ivonaldo",
+        sobrenome: "Soares"
+      },
+      {
+        nome: "Maria",
+        sobrenome: "Silva"
+      },
+      {
+        nome: "João",
+        sobrenome: "Pereira"
+      },
+      {
+        nome: "Marcio",
+        sobrenome: "Santos"
+      },
+    ]
+
+    constructor() {
+    }
+
+    ngOnInit() {
+      let interval = setInterval(() => {
+        this.count++;
+        if (this.count === 10){
+          clearInterval(interval);
+        }
+      }, 2000)
+    }
+
+    /*private clicou(nome: string): void{
+      console.log('Clicou em min', nome)
+    }*/
+
 }
